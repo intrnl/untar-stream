e3807e15eb2356c1048a308aa000c3936be5fd75
intrnl <intrnl28+git@gmail.com>
2022-11-24 17:10:33 +0700

    chore: test on browser

---
+++ test/browser.js
@@ -0,0 +1,41 @@
+import { Untar } from '../src/index.js';
+
+/** @type {HTMLInputElement} */
+let input = document.getElementById('input');
+/** @type {HTMLUListElement} */
+let list = document.getElementById('list');
+/** @type {AbortController | null} */
+let controller = null;
+
+input.addEventListener('change', () => {
+	let file = input.files[0];
+
+	if (controller) {
+		controller.abort();
+	}
+
+	controller = new AbortController();
+	retrieveTarListing(file, controller.signal);
+});
+
+/**
+ * @param {File} file
+ * @param {AbortSignal} signal
+ */
+async function retrieveTarListing (file, signal) {
+	let stream = file.stream();
+	let untar = new Untar(stream);
+
+	list.innerHTML = '';
+
+	for await (let entry of untar) {
+		if (signal.aborted) {
+			break;
+		}
+
+		let li = document.createElement('li');
+		li.textContent = `${entry.name} (${entry.size} b)`;
+
+		list.appendChild(li);
+	}
+}
